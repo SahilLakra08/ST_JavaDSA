2b56d0255ac1b084fa9e5bf270dff032ff8bcf6b
unknown <lakra08sahil@gmail.com>
2024-08-02 15:02:56 +0530

    oops&calls

---
+++ JavaDsa/Week3/OOPS_Concepts/Calls.java
@@ -0,0 +1,14 @@
+package ST_JavaDSA.JavaDsa.Week3.OOPS_Concepts;
+
+public class Calls {
+    public static void main(String[] args) {
+        Oops obj1=new Oops();
+        System.out.println(obj1.num);
+        Oops obj2=new Oops();
+        System.out.println(obj2.num);
+        Oops obj3=new Oops();
+        System.out.println(obj3.num);
+        System.out.println(obj1.num);
+        System.out.println(Oops.num);
+    }
+}
---
+++ JavaDsa/Week3/OOPS_Concepts/Oops.java
@@ -0,0 +1,15 @@
+package ST_JavaDSA.JavaDsa.Week3.OOPS_Concepts;
+
+public class Oops {
+    static int num=10;
+    public Oops(){
+        num++;
+    }
+    public static class Inner{
+        public Inner(){}
+            public static class Inner2{
+                public Inner2(){}
+            }        
+         }
+    }
+
